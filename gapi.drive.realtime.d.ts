8ea3324fcc3304dc27c80df5abf443dc333f722f
grapswiz <ineeza@gmail.com>
2014-05-08 00:33:43 +0900

    add Class gapi.drive.realtime.Model

--- gapi.drive.realtime.d.ts
+++ gapi.drive.realtime.d.ts
@@ -184,4 +184,123 @@ declare module gapi.drive.realtime {
          */
         removeEventListener(type:string, handler:Object, opt_capture:boolean);
     }
+
+    /**
+     * The collaborative model is the data model for a Realtime document. The document's object graph should be added to the model under the root object. All objects that are part of the model must be accessible from this root.
+     * The model class is also used to create instances of built in and custom collaborative objects via the appropriate create method.
+     * Listen on the model for the following events: gapi.drive.realtime.EventType.UNDO_REDO_STATE_CHANGED
+     * This class should not be instantiated directly. The collaborative model is generated during the document load process. The model can be initialized by passing an initializer function to gapi.drive.realtime.load.
+     */
+    export class Model {
+        /**
+         * * The collaborative model is the data model for a Realtime document. The document's object graph should be added to the model under the root object. All objects that are part of the model must be accessible from this root.
+         * The model class is also used to create instances of built in and custom collaborative objects via the appropriate create method.
+         * Listen on the model for the following events: gapi.drive.realtime.EventType.UNDO_REDO_STATE_CHANGED
+         * This class should not be instantiated directly. The collaborative model is generated during the document load process. The model can be initialized by passing an initializer function to gapi.drive.realtime.load.
+         * @param bridge The driver for the GWT collaborative libraries.
+         * @param document The document that this model belongs to.
+         */
+        constructor(bridge:any, document:any); //TODO gapi.drive.realtime.GwtDocumentBridge gapi.drive.realtime.Document
+        /**
+         * Adds an event listener to the event target. The same handler can only be added once per the type. Even if you add the same handler multiple times using the same type then it will only be called once when the event is dispatched.
+         * @param type The type of the event to listen for.
+         * @param handler The function to handle the event.
+         * @param opt_capture In DOM-compliant browsers, this determines whether the listener is fired during the capture or bubble phase of the event.
+         */
+        addEventListener(type:string, handler:Function, opt_capture:boolean);
+        /**
+         * Adds an event listener to the event target. The same handler can only be added once per the type. Even if you add the same handler multiple times using the same type then it will only be called once when the event is dispatched.
+         * @param type The type of the event to listen for.
+         * @param handler The handler can also be an object that implements the handleEvent method which takes the event object as argument.
+         * @param opt_capture In DOM-compliant browsers, this determines whether the listener is fired during the capture or bubble phase of the event.
+         */
+        addEventListener(type:string, handler:Object, opt_capture:boolean);
+        /**
+         * Removes an event listener from the event target. The handler must be the same object as the one added. If the handler has not been added then nothing is done.
+         * @param type The type of the event to listen for.
+         * @param handler The function to handle the event.
+         * @param opt_capture In DOM-compliant browsers, this determines whether the listener is fired during the capture or bubble phase of the event.
+         */
+        removeEventListener(type:string, handler:Function, opt_capture:boolean);
+        /**
+         * Removes an event listener from the event target. The handler must be the same object as the one added. If the handler has not been added then nothing is done.
+         * @param type The type of the event to listen for.
+         * @param handler  The handler can also be an object that implements the handleEvent method which takes the event object as argument.
+         * @param opt_capture In DOM-compliant browsers, this determines whether the listener is fired during the capture or bubble phase of the event.
+         */
+        removeEventListener(type:string, handler:Object, opt_capture:boolean);
+
+        /**
+         * Starts a compound operation. If a name is given, that name will be recorded in the mutation for use in revision history, undo menus, etc. When beginCompoundOperation() is called, all subsequent edits to the data model will be batched together in the undo stack and revision history until endCompoundOperation() is called. Compound operations may be nested inside other compound operations. Note that the compound operation MUST start and end in the same synchronous execution block. If this invariant is violated, the data model will become invalid and all future changes will fail.
+         * @param opt_name An optional name for this compound operation.
+         */
+        beginCompoundOperation(opt_name:string);
+
+        /**
+         * Creates and returns a new collaborative object. This can be used to create custom collaborative objects. For built in types, use the specific create* functions.
+         * @param ref An object constructor.
+         * @param var_args Arguments to the newly-created object's initialize() method.
+         * @return A new collaborative object.
+         */
+        create(ref:Function, var_args:any):Object;
+
+        /**
+         * Creates a collaborative list.
+         * @param opt_initialValue Initial value for the list.
+         * @return A collaborative list.
+         */
+        createList(opt_initialValue:any[]):Object;
+
+        /**
+         * Creates a collaborative map.
+         * @param opt_initialValue Initial value for the map.
+         * @return A collaborative map.
+         */
+        createMap(opt_initialValue:Object):Object;
+
+        /**
+         * Creates a collaborative string.
+         * @param opt_initialValue Sets the initial value for this string.
+         * @return A collaborative string.
+         */
+        createString(opt_initialValue:string):Object;
+        /**
+         * Ends a compound operation. This method will throw an exception if no compound operation is in progress.
+         */
+        endCompoundOperation():void;
+        /**
+         * Returns the root of the object model.
+         * @return The root of the object model.
+         */
+        getRoot():any; //TODO gapi.drive.realtime.CollaborativeMap
+        /**
+         * Returns whether the model is initialized.
+         * @return Whether the model is initialized.
+         */
+        isInitialized():boolean;
+        /**
+         * Redo the last thing the active collaborator undid.
+         */
+        redo():void;
+        /**
+         * Undo the last thing the active collaborator did.
+         */
+        undo():void;
+        /**
+         * An estimate of the number of bytes used by data stored in the model.
+         */
+        bytesUsed:number;
+        /**
+         * True if the model can currently redo.
+         */
+        canRedo: boolean;
+        /**
+         * True if the model can currently undo.
+         */
+        canUndo: boolean;
+        /**
+         * The mode of the document. If true, the document is readonly. If false it is editable.
+         */
+        isReadOnly: boolean;
+    }
 }
